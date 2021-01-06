98fea9bb7c9eba97aa42db7c41b553bd52aacc67
turtlelazy <ishraqmahid@gmail.com>
2021-01-06 12:06:42 -0500

    get methods created

--- Node.java
+++ Node.java
@@ -1,8 +1,21 @@
 public class Node {
-    public Node(String value) {
-        /* create a constructor */}
-
     private String data;
     private Node next, prev;
-    // write get/set methods for all three instance variables.
+    public Node(String value) {
+        data = value;
+    }
+    
+    public Node getNext(){
+        return next;
+    }
+
+    public Node getPrev(){
+        return prev;
+    }
+
+    public String getData(){
+        return data;
+    }
+
+
 }
